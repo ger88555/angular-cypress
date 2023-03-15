129210373288b5da38c0b4d2fc187e752ab354ad
ger88555 <germanadolf@protonmail.com>
2023-03-14 23:27:09 -0700

    feat(custom-input): add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CustomInputComponent } from './custom-input/custom-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomInputComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/custom-input/custom-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-input',
+  templateUrl: './custom-input.component.html',
+  styleUrls: ['./custom-input.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CustomInputComponent {
+  static nextID = 0;
+  nativeID = CustomInputComponent.nextID++;
+  
+  value = '';
+}
